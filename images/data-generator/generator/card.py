3fec58c496b4231221f6d6faddb79ae7c8f042e7
dan <danbaron63@gmail.com>
2026-08-23 18:40:57 +0800

    data-generator enforced MAX_CARDS_PER_ACCOUNT in card and reformatted account

--- images/data-generator/generator/card.py
+++ images/data-generator/generator/card.py
@@ -3,7 +3,7 @@ from datetime import UTC, datetime
 
 from generator.base import BaseDatabase, BaseEntity
 
-max_cards_per_account = 2
+MAX_CARDS_PER_ACCOUNT = 2
 
 
 @dataclass
@@ -15,14 +15,24 @@ class Card(BaseEntity):
 
 
 class CardDatabase(BaseDatabase):
-    def __init__(self, customer_db, account_db):
+    def __init__(self, account_db):
         super().__init__(Card)
-        self._customer_db = customer_db
         self._account_db = account_db
+        self._card_count_query = (
+            f"SELECT count(*) FROM {self._entity_type.__name__} WHERE account_id = ?"
+        )
 
     def card_issued(self) -> Card:
         # get customer
         account = self._account_db.get_random()
+
+        # get cards for this account
+        result = self._persistence.query(self._card_count_query, [account.id])
+        no_of_cards = result[0][0]
+
+        if no_of_cards >= MAX_CARDS_PER_ACCOUNT:
+            # fetch a different one...
+            return self.card_issued()
 
         card = Card(
             id=self._get_next_id(),
